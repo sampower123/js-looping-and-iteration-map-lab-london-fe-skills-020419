3b1dd23f2cd729c2cc139b3baa7b9e87d2c94988
Sam Power <sam.g.power@gmail.com>
2019-03-18 20:45:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file.
+
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+let lowerDriver = drivers.map(function(driver) {
+  return driver.toLowerCase();
+});
+
+function lowerCaseDrivers() {
+  return lowerDriver;
+}
+
+let fullName = ['Bobby Smith', 'Sammy Watkins', 'Sally Jenkins', 'Annette Sawyer', 'Sarah Hucklebee', 'bobby ridge'];
+
